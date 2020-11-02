4a1e6acedfdde3fe5ba56986ecef7d50bb5ecee4
Gilberto Filho <ggafbr@gmail.com>
2020-11-02 14:02:10 -0500

    Extraindo SECRET_KEY para dentro do .env
    close #14

--- pypro/settings.py
+++ pypro/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'g*#th1rsfrqj1s!nf3!2acde)jtz24#h+3@3&pu*p5#j4889-e'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
