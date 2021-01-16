46e29d03c96e1e13aea3ef29c158933f79dc3036
gilberto <ggafbr@gmail.com>
2021-01-16 09:49:03 -0500

    Primeira versao da Landing Page

--- pypro/base/tests/test_home.py
+++ pypro/base/tests/test_home.py
@@ -1,8 +1,9 @@
 import pytest
-from django.test import Client
+# from django.test import Client
 from django.urls import reverse
 
 from pypro.django_assertions import assert_contains
+
 
 @pytest.fixture
 def resp(client):
