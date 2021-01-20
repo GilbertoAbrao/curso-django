548860b7154c5ba067128c1d13d4d08800d5a522
gilberto <ggafbr@gmail.com>
2021-01-20 09:06:43 -0500

    Antes de começar em banco de dados

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -1,19 +1,10 @@
 from django.shortcuts import render
-from django.urls import reverse
 
-
-class Video:
-    def __init__(self, slug, titulo, vimeo_id):
-        self.slug = slug
-        self.titulo = titulo
-        self.vimeo_id = vimeo_id
-
-    def get_absolute_url(self):
-        return reverse('aperitivos:video', args=(self.slug,))
+from pypro.aperitivos.models import Video
 
 videos = [
-    Video('motivacao', 'Video Aperitivo: Motivação', 'nEiAdLQCKD0'),
-    Video('instalacao-windows', 'Instalação Windows', '251224475'),
+    Video(slug='motivacao', titulo='Video Aperitivo: Motivação', vimeo_id='nEiAdLQCKD0'),
+    Video(slug='instalacao-windows', titulo='Instalação Windows', vimeo_id='251224475'),
 ]
 
 videos_dct = {v.slug: v for v in videos}
