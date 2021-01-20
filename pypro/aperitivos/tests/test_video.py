2954caaf2edc55ed8f87b6025e1ece0c98f34867
gilberto <ggafbr@gmail.com>
2021-01-20 12:31:17 -0500

    Com modelo e com acesso a dados

--- pypro/aperitivos/tests/test_video.py
+++ pypro/aperitivos/tests/test_video.py
@@ -1,6 +1,7 @@
 import pytest
 
 from django.urls import reverse
+from model_mommy import mommy
 
 from pypro.aperitivos.models import Video
 from pypro.django_assertions import assert_contains
@@ -8,9 +9,7 @@ from pypro.django_assertions import assert_contains
 
 @pytest.fixture
 def video(db):
-    v = Video(slug='motivacao', titulo='Video Aperitivo: Motivação', vimeo_id='nEiAdLQCKD0')
-    v.save()
-    return v
+    return mommy.make(Video)
 
 
 @pytest.fixture
@@ -18,8 +17,17 @@ def resp(client, video):
     return client.get(reverse('aperitivos:video', args=(video.slug,)))
 
 
+@pytest.fixture
+def resp_video_nao_encontrado(client, video):
+    return client.get(reverse('aperitivos:video', args=(video.slug+'video_nao_existente',)))
+
+
 def test_status_code(resp):
     assert resp.status_code == 200
+
+
+def test_status_code_video_nao_encontrado(resp_video_nao_encontrado):
+    assert resp_video_nao_encontrado.status_code == 404
 
 
 def test_titulo_video(resp, video):
